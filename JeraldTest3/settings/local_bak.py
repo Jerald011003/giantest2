0d71dcaa11436c0ba9f8ded320d79eac42b6b21d
jerald011003 <coninegaming@gmail.com>
2022-11-12 06:32:17 +0800

    2nd

--- JeraldTest3/settings/local_bak.py
+++ JeraldTest3/settings/local_bak.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [BASE_DIR / "static_my_project",]
+STATICFILES_DIRS = [BASE_DIR / "static_my_project", ]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
 
